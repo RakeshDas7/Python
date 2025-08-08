d674719bb971e8e6eddca160e4abb2aac8dfb275
RakeshDas7 <rakeshdas1477@gmail.com>
2025-08-08 09:59:43 +0530

    Operator & TypeCasting in Python

---
+++ Day 3 Operators/Calculator.py
@@ -0,0 +1,10 @@
+a = 50
+b = 3
+
+print("The value of", a, "+", b, "=", a + b)
+print("The value of", a, "-", b, "=", a - b)
+print("The value of", a, "*", b, "=", a * b)
+print("The value of", a, "/", b, "=", a / b)
+print("The value of", a, "//", b, "=", a // b)
+print("The value of", a, "**", b, "=", a ** b)
+print("The value of", a, "reminder", b, "=", a % b)
---
+++ Day 3 Operators/TypeCasting.py
@@ -0,0 +1,16 @@
+a = 25 #integere type
+b = "5" #String type
+c = int(b) #int type -> This is called type casting
+print(a + int(b)) # Output: 30
+
+print(str(a) + b) # Output: 255
+
+# The conversion of one data type to another data type is know as TypeCasting
+# TypeCasting is used to convert the data type of a variable from one data type to another
+
+#Implicit Typecasting
+a = 6.8
+b = 5
+print( a + b) # Output: 11.8
+
+#Implicit Typecasting -> Lower data type to higher data type conversion automatically done by the python
